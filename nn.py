40b56e19a0bee25ab6b9786ce6eaabc4faf9f7eb
zykov-vv <74975689+zykov-vv@users.noreply.github.com>
2020-11-24 20:20:56 +0500

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import *
+import os
 
 app = Flask(__name__)
 
@@ -111,5 +112,7 @@ def form_sample():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    #app.run(port=8080, host='127.0.0.1')
     #app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
